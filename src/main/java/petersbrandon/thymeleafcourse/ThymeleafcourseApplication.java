2dcb2d0238db677433eac15755359cba9b0e0293
bmpeters <peters.brandonm@gmail.com>
2020-02-20 16:55:28 -0700

    Initial Project Add

---
+++ src/main/java/petersbrandon/thymeleafcourse/ThymeleafcourseApplication.java
@@ -0,0 +1,14 @@
+package petersbrandon.thymeleafcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafcourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafcourseApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/petersbrandon/thymeleafcourse/ThymeleafcourseApplicationTests.java
@@ -0,0 +1,14 @@
+package petersbrandon.thymeleafcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafcourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+}
